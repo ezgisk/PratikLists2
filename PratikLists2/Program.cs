02cd78d92bcaabb1266003268b686b2b52bfd502
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-29 12:22:08 -0700

    Add project files.

---
+++ PratikLists2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Specialized;
+
+Console.WriteLine("Lutfen kahve adi girin:");
+
+List<string> list = new List<string>();//Creating a new list
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"{i}. Kahve:");
+    list.Add(Console.ReadLine());//Adding elements to list
+}
+Console.WriteLine("Istediginiz kahveler :");
+
+foreach (string s in list)
+{
+    Console.WriteLine(s);//Writing the list the console
+}
+Console.ReadKey();
+
